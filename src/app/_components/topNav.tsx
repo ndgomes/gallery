82ef04ad8aaa078a41c715194b723e63ed01f56c
Nuno Gomes <nuno98gomes@gmail.com>
2024-07-26 20:23:15 +0200

    Uploadthing added

--- src/app/_components/topNav.tsx
+++ src/app/_components/topNav.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../utils/uploadthing";
+import { useRouter } from "next/navigation";
 
 export function TopNav() {
+  const router = useRouter();
+
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
@@ -10,6 +16,10 @@ export function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton
+            endpoint="imageUploader"
+            onClientUploadComplete={() => router.refresh()}
+          />
           <UserButton />
         </SignedIn>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 
 import { TopNav } from "./_components/topNav";
+
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { ourFileRouter } from "./api/uploadthing/code";
+import { extractRouterConfig } from "uploadthing/server";
 
 export const metadata: Metadata = {
   title: "Galery App",
@@ -18,6 +23,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
+        <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className="flex flex-col gap-4">
           <TopNav />
           {children}
